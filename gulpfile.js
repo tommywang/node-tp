49de2570bc4986dd32ba2be3a88e66ec6007e8bf
Shiyue Wang <shiyue.wang@me.com>
2016-05-13 09:50:15 +0200

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+var inject = require('gulp-inject');
 
-gulp.task('default', function() {
-    console.log('done!');
+gulp.task('socket.io', function () {
+  return gulp.src('./node_modules/socket.io-client/socket.io.js')
+    .pipe(gulp.dest('./public/vendors'));
 });
+
+gulp.task('inject', function () {
+  return gulp.src('./public/index.html')
+    .pipe(inject(gulp.src(['./public/app/**/*.js'], { read: false }), { relative: true }))
+    .pipe(wiredep())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', ['socket.io', 'inject']);
